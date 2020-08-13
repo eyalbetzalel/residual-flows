50e6368d5b123dd5abeaed5a585264a48d69345c
Eyal Betzalel <eyalbatzul@gmail.com>
2020-08-13 10:54:04 +0300

    fix dataloader

--- train_img.py
+++ train_img.py
@@ -381,9 +381,9 @@ elif args.data == 'imagenet64':
     transform=transforms.Compose([add_noise])
     path = r"/home/dsi/eyalbetzalel/glow/GLOW/GLOW/imagenet64/imagenet64"
     trainSet = vdsets.ImageFolder(path + "/train_64x64" , transform=transform)
-    train_loader = DataLoader(trainSet, shuffle=False, batch_size=args.batchsize, num_workers=args.nworkers)
+    train_loader = torch.utils.data.DataLoader(trainSet, shuffle=False, batch_size=args.batchsize, num_workers=args.nworkers)
     testSet = vdsets.ImageFolder(path + "/valid_64x64" , transform=transform)
-    test_loader = DataLoader(testSet, shuffle=False, batch_size=args.val_batchsize, num_workers=args.nworkers)
+    test_loader = torch.utils.data.DataLoader(testSet, shuffle=False, batch_size=args.val_batchsize, num_workers=args.nworkers)
     print("Finish Load ImageNet")
 #    train_loader = torch.utils.data.DataLoader(
 #        datasets.Imagenet64(train=True, transform=transforms.Compose([

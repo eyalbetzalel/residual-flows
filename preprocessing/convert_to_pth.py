f85f848f6d82a6a6f77657f56da5d83ed3dce54a
Eyal Betzalel <eyalbatzul@gmail.com>
2020-08-15 20:56:04 +0300

    Update convert_to_pth.py

--- preprocessing/convert_to_pth.py
+++ preprocessing/convert_to_pth.py
@@ -3,6 +3,6 @@ import re
 import numpy as np
 import torch
 
-img = torch.tensor(np.load(sys.argv[1]))
+img = torch.tensor(np.load(sys.argv[1]), dtype=torch.float64)
 img = img.permute(0, 3, 1, 2)
 torch.save(img, re.sub('.npy$', '.pth', sys.argv[1]))
